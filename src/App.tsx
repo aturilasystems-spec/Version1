1b3d887058d5906984074e3cef85e7716ddd0ce8
aturilasystems-spec <aturilasystems@gmail.com>
2025-08-22 13:01:41 +0100

    Remove animated scroll arrow

--- src/App.tsx
+++ src/App.tsx
@@ -137,11 +137,6 @@ function App() {
             </div>
           </div>
         </div>
-        
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <ChevronDown size={32} className="opacity-60" />
-        </div>
       </section>
 
       {/* Services Section */}
